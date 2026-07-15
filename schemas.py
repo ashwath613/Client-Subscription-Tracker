404755ff35dd750076436f3c58703743766ed5ce
Ashwath N <nashwath613@gmail.com>
2026-07-15 17:01:48 +0530

    Add files via upload

--- schemas.py
+++ schemas.py
@@ -19,22 +19,15 @@ class SubscriptionStatus(str, Enum):
 # customer table as python class
 
 class CustomerCreate(BaseModel):
-
     name: str
-
     email: EmailStr
 
 # customer response model for visulaization
 class CustomerResponse(BaseModel):
-
     id: int
-
     name: str
-
     email: EmailStr
-
     created_at: datetime
-
     class Config:
         from_attributes = True
 
@@ -42,38 +35,24 @@ class CustomerResponse(BaseModel):
 # subscription table in python class
 
 class SubscriptionCreate(BaseModel):
-
     customer_id: int
-
     plan_type: PlanType
-
     status: SubscriptionStatus
-
     usage_limit: int
-
     billing_start: date
-
     billing_end: date
 
 #subscriptions response model
 class SubscriptionResponse(BaseModel):
 
     id: int
-
     customer_id: int
-
     plan_type: PlanType
-
     status: SubscriptionStatus
-
     usage_limit: int
-
     current_usage: int
-
     billing_start: date
-
     billing_end: date
-
     class Config:
         from_attributes = True
 
@@ -81,5 +60,4 @@ class SubscriptionResponse(BaseModel):
 # record usuage
 
 class UsageRequest(BaseModel):
-
     usage: int
